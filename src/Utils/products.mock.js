297afcf46a9f479f808b1fa01228ea3c3989d173
Miguel Angel Guajardo <miguelguajardo1468@hotmail.com>
2022-07-27 17:08:14 -0300

    Agregado del ItemList

---
+++ src/Utils/products.mock.js
@@ -0,0 +1,83 @@
+import CañaLexusChaiten from "../img/CañaLexusChaiten.jpg";
+import CañaSpinitRainForestII from "../img/CañaSpinitRainForestII.jpg";
+import CañaSaintCarbonTech from "../img/CañaSaintCarbonTech.png";
+import CañaWaterdogBlackGFeeder from "../img/CañaWaterdogBlackGFeeder.jpg";
+import CañaAlbatrosMarea from "../img/CañaAlbatrosMarea.jpg";
+import CañaTechFoxrot from "../img/CañaTechFoxrot.jpg";
+import CañaKunnanSilverside from "../img/CañaKunnanSilverside.jpg";
+import CañaKunnanSilverside400 from "../img/CañaKunnanSilverside400.jpg";
+import CañaShimanoFxC60mc26 from "../img/CañaShimanoFxC60mc26.jpg";
+import CañaSpinitTornado from "../img/CañaSpinitTornado.png";
+const products = [
+  {
+    id: 1,
+    name: "Caña Lexus Chaiten",
+    image: CañaLexusChaiten,
+    price: 15000,
+    stock: 10,
+  },
+  {
+    id: 2,
+    name: "Caña Spinit Rain Forest II",
+    image: CañaSpinitRainForestII,
+    price: 9960,
+    stock: 5,
+  },
+  {
+    id: 3,
+    name: "Caña Saint Carbon Tech",
+    image: CañaSaintCarbonTech,
+    price: 15000,
+    stock: 6,
+  },
+  {
+    id: 4,
+    name: "Caña Waterdog Black G Feeder",
+    image: CañaWaterdogBlackGFeeder,
+    price: 19500,
+    stock: 3,
+  },
+  {
+    id: 5,
+    name: "Caña Albatros Marea",
+    image: CañaAlbatrosMarea,
+    price: 12000,
+    stock: 8,
+  },
+  {
+    id: 6,
+    name: "Caña Tech Foxrot",
+    image: CañaTechFoxrot,
+    price: 25000,
+    stock: 7,
+  },
+  {
+    id: 7,
+    name: "Caña Kunnan Silverside 4.50",
+    image: CañaKunnanSilverside,
+    price: 9570,
+    stock: 8,
+  },
+  {
+    id: 8,
+    name: "Caña Kunnan Silverside 4.00",
+    image: CañaKunnanSilverside400,
+    price: 9240,
+    stock: 8,
+  },
+  {
+    id: 9,
+    name: "Caña Shimano Fx C60mc2 6 - 15 Lbs Pesca Baitcast Con Gatillo",
+    image: CañaShimanoFxC60mc26,
+    price: 5800,
+    stock: 12,
+  },
+  {
+    id: 10,
+    name: "Caña Spinit Tornado",
+    image: CañaSpinitTornado,
+    price: 12050,
+    stock: 6,
+  },
+];
+export default products;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,44 +1,30 @@
+import { useEffect, useState } from "react";
 import "./ItemListContainer.css";
-import ItemProduct from "../ItemProduct/ItemProduct";
-import CañaLexusChaiten from "../../img/CañaLexusChaiten.jpg";
-import CañaSpinitRainForestII from "../../img/CañaSpinitRainForestII.jpg";
-import CañaSaintCarbonTech from "../../img/CañaSaintCarbonTech.png";
-import CañaWaterdogBlackGFeeder from "../../img/CañaWaterdogBlackGFeeder.jpg";
+import products from "../../Utils/products.mock";
+import ItemList from "../ItemList/ItemList";
+
 export default function ItemListContainer() {
-  const product1 = {
-    id: 1,
-    name: "Caña Lexus Chaiten",
-    image: CañaLexusChaiten,
-    price: 15000,
-    stock: 10,
-  };
-  const product2 = {
-    id: 2,
-    name: "Caña Spinit Rain Forest II",
-    image: CañaSpinitRainForestII,
-    price: 9960,
-    stock: 5,
-  };
-  const product3 = {
-    id: 3,
-    name: "Caña Saint Carbon Tech",
-    image: CañaSaintCarbonTech,
-    price: 15000,
-    stock: 6,
-  };
-  const product4 = {
-    id: 4,
-    name: "Caña Waterdog Black G Feeder",
-    image: CañaWaterdogBlackGFeeder,
-    price: 19500,
-    stock: 3,
-  };
+  const [listProducts, setListProducts] = useState([]);
+  const getProducts = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 2000);
+  });
+  useEffect(() => {
+    getProducts
+      .then((res) => {
+        //console.log("Productos: ", res);
+        setListProducts(res);
+      })
+      .catch((error) => {
+        console.log("La llamada fallo");
+      })
+      .finally(() => {});
+  }, []);
+
   return (
     <div className="ItemListContainer">
-      <ItemProduct data={product1} />
-      <ItemProduct data={product2} />
-      <ItemProduct data={product3} />
-      <ItemProduct data={product4} />
+      <ItemList dataProducts={listProducts} />
     </div>
   );
 }
